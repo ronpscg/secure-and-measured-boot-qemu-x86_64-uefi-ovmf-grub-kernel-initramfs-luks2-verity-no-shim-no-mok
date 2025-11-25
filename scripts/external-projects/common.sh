cc9efc67426e76f9049f1d0cc6e16685efad9cf5
Ron Munitz <ron@thepscg.com>
2025-11-25 22:15:03 +0100

    wip common.sh

---
+++ scripts/external-projects/common.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Common file for paths etc.
+
+#
+# Underdocuented - but should be very clear for every bash reader... Maybe I will update it after I am done
+#
+
+set -a
+
+LOCAL_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+BUILD_TOP=$(readlink -f $LOCAL_DIR/../../)
+cd $LOCAL_DIR
+# Common project dir
+: ${REQUIRED_PROJECTS_DIR=$HOME/pscg/secureboot-qemu-x86_64-efi-grub/components}
+DL_DIR=$REQUIRED_PROJECTS_DIR
+# There is an intended reduandancy (e.g. to store initial EFI variables or initramfs or rootfs before modifying it) so you can directly copy to the ARTIFACTS_DIR if you want to.
+: ${REQUIRED_PROJECTS_ARTIFACTS_DIR=$REQUIRED_PROJECTS_DIR/artifacts} # These are the separate artifacts, to allow an easy place to store the build artifacts before modifying them
+: ${ARTIFACTS_DIR=$HOME/pscg/secureboot-qemu-x86_64-efi-grub/artifacts}	# This is where everything will be after its all built and signed, and where the final image will be made out of "debuging images"
+
+# Doing tarball now, can do git instead
+# all tarballs will just be downloaded directly into $REQUIRED_PROJECTS_DIR. Maybe it will be changed.
+# PscgBuildOS and some of my other work organizes things more nicely and ALWAYS separates the source from the build directory. Since here everything was hacked brutally quickly,
+# I am presenting the in folder way. Which is fine, as it is easier for most people either way
+
+KV=6.18-rc7
+KERNEL_BUILDER_DIR=${REQUIRED_PROJECTS_DIR}/linux-$KV
+KERNEL_CONFIG=$BUILD_TOP/kernel-configs/config-x86_64_tpm-dmcrypt-dmverity
+
+GRUB_BUILDER_DIR=${REQUIRED_PROJECTS_DIR}/
+
+EDK2_BULIDER_DIR=${REQUIRED_PROJECTS_DIR}/
+
+OVMF_BUILDER_DIR=${REQUIRED_PROJECTS_DIR}/
+
+INITRAMFS_BUILDER_DIR=${REQUIRED_PROJECTS_DIR}/dockers/initramfs-builder
+
