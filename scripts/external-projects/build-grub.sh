c3d0ee789c0c7bcda31904158f40658750ad08f7
Ron Munitz <ron@thepscg.com>
2025-11-27 00:03:05 +0100

    modified grub modules at the standalone binary

--- scripts/external-projects/build-grub.sh
+++ scripts/external-projects/build-grub.sh
@@ -23,7 +23,8 @@ build_grub_core() (
 build_standalone_image() (
 	cd $GRUB_BUILDER_DIR
 	./grub-mkstandalone -O x86_64-efi -o grubx64.efi --directory=./grub-core  \
-		--modules="part_gpt part_msdos fat ext2 normal boot linux configfile"   --fonts="" --locales="" --themes=""  \
+		--modules="part_gpt part_msdos ext2 linux normal boot configfile search ls cat echo test tpm2_measure"
+			--fonts="" --locales="" --themes=""  \
 		"boot/grub/grub.cfg=$GRUB_CONFIG"
 )
 
