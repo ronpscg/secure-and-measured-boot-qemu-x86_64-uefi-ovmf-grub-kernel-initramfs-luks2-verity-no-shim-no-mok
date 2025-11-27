c1e281cf904ed457389cdce25722abab9e87e3bb
Ron Munitz <ron@thepscg.com>
2025-11-27 15:18:04 +0100

    moved qemu and swtpm scripts to the qemu folder

--- scripts/qemu/test-tmp.sh
+++ scripts/qemu/test-tmp.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-DIR2=/home/ron/pscg/secureboot-qemu-x86_64-efi-grub/play
+. ../common.sh || { echo "Please run this script from the correct place" ; exit 1 ; }
 
-
-#export OVMF_CODE=/home/ron/pscg/secureboot-qemu-x86_64-efi-grub/edk2/Build/OvmfX64/RELEASE_GCC/FV/OVMF_CODE.fd
-#export OVMF_VARS=${DIR}/OVMF_VARS_custom.fd
-
-. ../common.sh
 DIR=$ARTIFACTS_DIR
 export OVMF_CODE=$DIR/OVMF_CODE.fd
 export OVMF_VARS=$DIR/OVMF_VARS.fd
 export FAT_ESP_FS_DIR=$ESP_FS_FOLDER 
+
 # Unencrypted
 #export ROOTFS_IMG=$DIR/rootfs.img
 # Encrypted
 export ROOTFS_IMG=$DIR/rootfs.enc.img
 export DMVERITY_HASH_IMG=${DIR}/dmverity-hash.img 
-export TPM_STATE_DIR=${DIR}/tpm-state
 
+export TPM_STATE_DIR=${DIR}/tpm-state
 
 export GPT_COMBINED_DISK_IMG=$DIR/usb_image.hdd.img
 
