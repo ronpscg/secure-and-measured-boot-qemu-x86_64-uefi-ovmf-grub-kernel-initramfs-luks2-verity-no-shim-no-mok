9f990682b934a0356db5ca7742e97f1924717c68
Ron Munitz <ron@thepscg.com>
2025-12-10 22:00:33 +0100

    docker: Dockerfile.yocto and building and running scripts (the latter could easily be merged with the other scripts). The idea is to also support mounting of SSTATE_MIRRORS or SSTATE_CACHE and some build materials. The are given the same paths as those I have on my host, merely so I can test them faster
    I could merge them, as only one image is required and one depends on the other. The nice thing about the respective Dockerfile.yocto is that it shows the minimum dependecies needed to do both together, and also shows how to construct a testing workflow that is independent, but makes a good basic block.

---
+++ docker/run-yocto-docker.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+LOCAL_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+: ${USER_NAME=user}
+if [ "$USER_NAME" = "root" ] ; then
+	: ${HOMEDIR=/root}
+else
+	: ${HOMEDIR=/home/$USER_NAME}
+fi
+
+: ${BINDMOUNTS=$LOCAL_DIR/bindmounts}
+: ${COPYGPGKEYS_ON_NEW_BINDMOUNTS_DIR=false} # if true, and there is a copy from a currently running dir in this repo (could do also outside but won't do so), also copy the keys from it
+
+#-----------------------------------------
+# Checks and initial setup help the user set up a directory so the current (git version controlled) working directory won't be bloated with the docker mounts  but don't interfere too much
+#-----------------------------------------
+if [ ! -d "$BINDMOUNTS" ] ; then
+	echo "Helping you to setup the $BINDMOUNTS directory, from the contents of source controlled $LOCAL_DIR"
+	mkdir -p $BINDMOUNTS || { echo "Cannot create directory $BINDMOUNTS" ; exit 1 ; }
+	set -euo pipefail
+	mkdir $BINDMOUNTS/{setup,homedir}
+	cp -va $LOCAL_DIR/bindmounts/setup/* $BINDMOUNTS/setup/
+	cp $LOCAL_DIR/bindmounts/homedir/entry.sh $BINDMOUNTS/homedir/
+	set +euo pipefail
+	# Won't copy the rest of the contens of the homedir as it could be huge, if it has gone previous builds
+	# But will be nice enough to keep previous customizations, and history, if the user had such
+	# don't worry about the warnings of not copying directories, if there are, as it is intentional
+	cp -v $LOCAL_DIR/bindmounts/homedir/.* $BINDMOUNTS/homedir/
+
+	if [ "$COPYGPGKEYS_ON_NEW_BINDMOUNTS_DIR" = "true" ] ; then
+		if [ -d $LOCAL_DIR/bindmounts/homedir/.gnupg ] ; then
+			cp -av $LOCAL_DIR/bindmounts/homedir/.gnupg $BINDMOUNTS/homedir/
+		fi
+	fi
+fi
+if [ ! -f "$BINDMOUNTS/homedir/entry.sh" ] ; then
+	echo "Please make sure you have set $BINDMOUNTS/homedir correctly. If you are using the folder from inside the repo at $LOCAL_DIR, you may want to reset your repo"
+	exit 1
+fi
+
+
+#-----------------------------------------
+# Running docker
+#-----------------------------------------
+: ${MOREMOUNTS=""}
+MOREMOUNTS+=" -v $HOME/pscg/customers/build-stuff/:$HOMEDIR/pscg/customers/build-stuff/" 
+PRIVILEGED=--privileged # To allow dealing with internal docker mounts and with losetup
+docker run -it --rm  $PRIVILEGED -v /var/run/docker.sock:/var/run/docker.sock \
+	--group-add $(getent group docker | awk -F: '{print $3}')  \
+	--group-add $(getent group kvm | awk -F: '{print $3}') \
+	-v ${BINDMOUNTS}/setup:/setup/ -v  ${BINDMOUNTS}/homedir:$HOMEDIR \
+	${MOREMOUNTS} \
+	-u $USER_NAME -e USER=$USER_NAME -w ${HOMEDIR}  \
+	wip-yocto-docker-secboot-builder:latest "$@"
