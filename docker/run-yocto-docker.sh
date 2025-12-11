e5e93d98c27eaa1af6af1abd7dec25d6f9e930c8
Ron Munitz <extromu@bang-olufsen.dk>
2025-12-11 22:09:50 +0100

    docker: added ways to speed up builds

--- docker/run-yocto-docker.sh
+++ docker/run-yocto-docker.sh
@@ -10,6 +10,9 @@ fi
 : ${BINDMOUNTS=$LOCAL_DIR/bindmounts}
 : ${COPYGPGKEYS_ON_NEW_BINDMOUNTS_DIR=false} # if true, and there is a copy from a currently running dir in this repo (could do also outside but won't do so), also copy the keys from it
 
+: ${MOREMOUNTS=""}
+MOREMOUNTS+=" -v $HOME/pscg/customers/build-stuff/:$HOMEDIR/pscg/customers/build-stuff/" 
+
 #-----------------------------------------
 # Checks and initial setup help the user set up a directory so the current (git version controlled) working directory won't be bloated with the docker mounts  but don't interfere too much
 #-----------------------------------------
@@ -37,12 +40,30 @@ if [ ! -f "$BINDMOUNTS/homedir/entry.sh" ] ; then
 	exit 1
 fi
 
+if [ -n "$MOREMOUNTS" ] ; then
+	# You should do this for every mount target (either here or inside the running Docker)- if the directory structure does not exist and is automatically created,
+	# it will be owned by the superuser, and you won't be able to write into it
+	mountdirs_srcs="$HOME/pscg/customers/build-stuff/"
+	mountdirs_targets="$BINDMOUNTS/homedir/pscg/customers/build-stuff/"
+	for dir in $mountdirs_srcs $mountdirs_targets ; do
+		if [ ! -d $dir ] ; then
+			echo Creating $dir for the first time
+			mkdir -p $dir
+		else
+			: # Won't change permissions. If the user decided to mix and match things, it's their problem
+		fi
+	done
+fi
+#--------- this is temporary, to avoid adding git keys on a disconnected computer ----------
+if [ -d ~/kas-project-src/ -a ! -d $BINDMOUNTS/homedir/yocto/kasdir ] ; then
+       mkdir $BINDMOUNTS/homedir/yocto
+	cp -av ~/kas-project-src/ $BINDMOUNTS/homedir/yocto/kasdir
+fi	
+#------------- end of temporary -------------
 
 #-----------------------------------------
 # Running docker
 #-----------------------------------------
-: ${MOREMOUNTS=""}
-MOREMOUNTS+=" -v $HOME/pscg/customers/build-stuff/:$HOMEDIR/pscg/customers/build-stuff/" 
 PRIVILEGED=--privileged # To allow dealing with internal docker mounts and with losetup
 docker run -it --rm  $PRIVILEGED -v /var/run/docker.sock:/var/run/docker.sock \
 	--group-add $(getent group docker | awk -F: '{print $3}')  \
@@ -50,4 +71,5 @@ docker run -it --rm  $PRIVILEGED -v /var/run/docker.sock:/var/run/docker.sock \
 	-v ${BINDMOUNTS}/setup:/setup/ -v  ${BINDMOUNTS}/homedir:$HOMEDIR \
 	${MOREMOUNTS} \
 	-u $USER_NAME -e USER=$USER_NAME -w ${HOMEDIR}  \
+	--name "yocto-builder-docker" \
 	wip-yocto-docker-secboot-builder:latest "$@"
