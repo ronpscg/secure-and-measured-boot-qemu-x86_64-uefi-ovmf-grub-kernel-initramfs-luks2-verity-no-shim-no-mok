b4367debbb8050be832534b397846fe1b7f2f817
Ron Munitz <ron@thepscg.com>
2025-12-17 13:53:00 +0100

    refactoring ext4 creation - datafs and bootfs - adding common code (not tested)
    Will not do it for the rootfs now, as there are some dependencies in the
    size output (which is fine, and which could be calculated again (du -b)
    before calling LUKS and dmverity. Perhaps after testing the bootfs and
    datafs refactoring.

--- scripts/main/make-images-bootfs.sh
+++ scripts/main/make-images-bootfs.sh
@@ -5,49 +5,12 @@ LOCAL_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 cd $LOCAL_DIR
 
 : ${BOOTFS_IMG=$ARTIFACTS_DIR/bootfs.img}
-
 : ${DONT_RECREATE_BOOTFS=false}
-if [ ! "$DONT_RECREATE_BOOTFS" = "true" ] ; then
-	echo "[+] Creating the bootfs image (where the Linux boot materials will reside)"
-	if [ -f $BOOTFS_IMG ] ; then
-		echo "WARNING: bootfs.img existed. Would you like to remove it? [y/n/^C]"
-		read
-		case $REPLY in
-			y|Y)
-				rm $BOOTFS_IMG
-				;;
-			""|n)
-				echo "Keeping the current bootfs image - be sure you know what you are doing"
-				;;
-			*)
-				exit 1
-				;;
-		esac	
-	fi
+: ${BOOTFS_IMG_SIZE=""}
 
 
-
-	BOOTFS_SIZE_MIB=$(echo "$(( ($(sudo du -sb $BOOT_FS_FOLDER  | cut  -f 1) ) )) * 1.4 / 1024/1024 + 1" | bc) # size of the current folder +40% for metadata and some extra working space
-	BOOTFS_MOUNT=$ARTIFACTS_DIR/bootfs.mount
-	fallocate -l ${BOOTFS_SIZE_MIB}MiB $BOOTFS_IMG
-	# would be better to check for existence, in previous scripts etc., but if something doesn't check out it's easy to trace to that, and unmount/losetup -d etc. manually and I simply don't have the time for that now
-	mkdir $BOOTFS_MOUNT
-	LOOPDEV=$(losetup -f)
-	sudo losetup -Pf $BOOTFS_IMG
-	sudo mkfs.ext4 $LOOPDEV
-	sudo mount $LOOPDEV $BOOTFS_MOUNT
-	sudo cp -a $BOOT_FS_FOLDER/* $BOOTFS_MOUNT
-	# maybe also copy ./... but in our debootstrap for now there won't be any under the root filesystem so it's simpler to follow
-
-	sudo umount $BOOTFS_MOUNT
-	sudo e2fsck -f $LOOPDEV
-	sudo resize2fs $LOOPDEV
-	sudo losetup -d $LOOPDEV
-	rmdir $BOOTFS_MOUNT
-	sync
-
-	echo "OK"
-else
-	BOOTFS_SIZE_MIB=$(echo "$(( ($(sudo du -sb $BOOTFS_IMG  | cut  -f 1) ) )) * 1.4 / 1024/1024 + 1" | bc)
-	echo "Using $BOOTFS_IMG. Size: $BOOTFS_SIZE_MIB MiB"
-fi
+main() {
+	. $LOCAL_DIR/make-images-ext4-common.sh 
+	create_ext4_image_from_folder $BOOT_FS_FOLDER $BOOTFS_IMG datafs $DONT_RECREATE_BOOTFS $BOOTFS_IMG_SIZE
+}
+main $@
---
+++ scripts/main/make-images-ext4-common.sh
@@ -0,0 +1,74 @@
+#
+# $1: src folder
+# $2: dst image file name
+# $3: name # for now just used to create a mountpoint, can be decided if we want to give it to some LABEL etc. PscgBuildOS does it nicer
+# $4: true to not recreate the file (and then do nothing) false otherwise
+# $5: size of target FS. If 0 - size will be decided accorduing to the src folder
+create_ext4_image_from_folder() {
+	local src=$1
+	local dst=$2
+	local mountname=$3 # can be changed
+	local dontrecreate=$4
+	local size=${5-0}
+
+	# keeping names in capital letters to be as identical as possible to the other scripts that do the exact same logic for diff purposes
+	local SIZE_MIB
+	local MOUNTPOINT
+
+	if [ ! "$dontrecreate" = "true" ] ; then
+		echo "[+] Creating the $(basename $dst) image"
+		if [ -f $dst ] ; then
+			echo "WARNING: $(basename $dst) existed. Would you like to remove it? [y/n/^C]"
+			read
+			case $REPLY in
+				y|Y)
+					rm $dst
+					;;
+				""|n)
+					echo "Keeping the current $(basename $dst) image - be sure you know what you are doing"
+					;;
+				*)
+					exit 1
+					;;
+			esac	
+		fi
+
+		if [ ! -d "$src" ] ; then
+			echo "[+] Creating $src for the first time, and assuming you are looking to create an empty file system of size $size"
+			mkdir -p $src
+		fi
+
+		if [ "$size" = "0" ] ; then
+			# If the source directory is empty, by default, on ext4 you will get:
+			# size  used avail    of
+			# 976K   24K  884K
+			# From which 16K will be lost+found
+			# This of course can change between systems if you have other default tunables for mkfs.ext4
+			SIZE_MIB=$(echo "$(( ($(sudo du -sb $src  | cut  -f 1) ) )) * 1.4 / 1024/1024 + 1" | bc) # size of the current folder +40% for metadata and some extra working space
+		else
+			SIZE_MIB=$size
+		fi
+
+		MOUNTPOINT=$ARTIFACTS_DIR/$mountname.mount
+		fallocate -l ${SIZE_MIB}MiB $dst
+		# would be better to check for existence, in previous scripts etc., but if something doesn't check out it's easy to trace to that, and unmount/losetup -d etc. manually and I simply don't have the time for that now
+		mkdir $MOUNTPOINT
+		LOOPDEV=$(losetup -f)
+		sudo losetup -Pf $dst
+		sudo mkfs.ext4 $LOOPDEV
+		sudo mount $LOOPDEV $MOUNTPOINT
+		sudo cp -aT $src $MOUNTPOINT  #  could rsync instead but systems do not come with rsync by default
+
+		sudo umount $MOUNTPOINT
+		sudo e2fsck -f $LOOPDEV
+		sudo resize2fs $LOOPDEV
+		sudo losetup -d $LOOPDEV
+		rmdir $MOUNTPOINT
+		sync
+
+		echo "OK"
+	else
+		SIZE_MIB=$(echo "$(( ($(sudo du -sb $dst  | cut  -f 1) ) )) * 1.4 / 1024/1024 + 1" | bc)
+		echo "Using $dst. Size: $SIZE_MIB MiB"
+	fi
+}
